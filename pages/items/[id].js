06f5f5a086ff2a6c9e3d012c42ef02b2898fa098
Dodam0719 <blueoh719@gmail.com>
2024-03-26 16:56:11 +0900

    🎨 Refactor: Image 컴포넌트 사용법 (외부이미지 사용시 next.config.js 설정)

--- pages/items/[id].js
+++ pages/items/[id].js
@@ -1,11 +1,12 @@
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import axios from '@/lib/axios';
-import styles from '@/styles/Product.module.css';
-import SizeReviewList from '@/components/SizeReviewList';
-import StarRating from '@/components/StarRating';
-import Header from '@/components/Header';
-import Container from '@/components/Container';
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import axios from "@/lib/axios";
+import styles from "@/styles/Product.module.css";
+import SizeReviewList from "@/components/SizeReviewList";
+import StarRating from "@/components/StarRating";
+import Header from "@/components/Header";
+import Container from "@/components/Container";
+import Image from "next/image";
 
 export default function Product() {
   const [product, setProduct] = useState();
@@ -43,8 +44,8 @@ export default function Product() {
           <span className={styles.englishName}>{product.englishName}</span>
         </h1>
         <div className={styles.content}>
-          <div>
-            <img className={styles.image} src={product.imgUrl} alt={product.name} />
+          <div className={styles.image}>
+            <Image fill src={product.imgUrl} alt={product.name} />
           </div>
           <div>
             <section className={styles.section}>
@@ -67,7 +68,7 @@ export default function Product() {
                       <td>
                         <span className={styles.salePrice}>
                           {product.price.toLocaleString()}원
-                        </span>{' '}
+                        </span>{" "}
                         {product.salePrice.toLocaleString()}원
                       </td>
                     </tr>
@@ -78,15 +79,14 @@ export default function Product() {
                     <tr>
                       <th>구매 후기</th>
                       <td className={styles.starRating}>
-                        <StarRating value={product.starRating} />{' '}
+                        <StarRating value={product.starRating} />{" "}
                         {product.starRatingCount.toLocaleString()}
                       </td>
                     </tr>
                     <tr>
                       <th>좋아요</th>
                       <td className={styles.like}>
-                        ♥
-                        {product.likeCount.toLocaleString()}
+                        ♥{product.likeCount.toLocaleString()}
                       </td>
                     </tr>
                   </tbody>
